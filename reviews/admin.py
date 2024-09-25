1694462ae8abad0051b3016860dc1ee23e202033
Anna Sahaidachna <anna.sahaidachna@gmail.com>
2024-09-25 16:21:00 +0000

    Update reviews view; add links for login/leave review on review page

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,9 +5,7 @@ from .models import Review
 @admin.register(Review)
 
 # Register your models here.
-
 class ReviewAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'score', 'approved', 'created_on')
-    summernote_fields = ('body')
     list_filter = ['created_on']
     
