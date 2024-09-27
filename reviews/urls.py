96bf278efad1410252e0fc4b0552bb9cd8a0e009
Anna Sahaidachna <anna.sahaidachna@gmail.com>
2024-09-27 13:35:42 +0000

    Fix delete button on review page

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.review_view, name='reviews'),
     path('reviews/edit_review/<int:review_id>',
          views.review_edit, name='review_edit'),
-    path('reviews/delete_review/<int:review_id>',
-         views.review_delete, name='review_delete'),
+    path('reviews/delete_review/<int:review_id>/', views.review_delete, name='review_delete'),
 ]
 
